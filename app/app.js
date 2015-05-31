17e62be0ab6d6015a531f36a35259aaf0b26aa02
Kyle Bradshaw <kyle@somedirection.com>
2015-05-30 22:39:55 -0400

    complete w styling

--- app/app.js
+++ app/app.js
@@ -14,5 +14,23 @@ function CalendarCtrl($scope) {
 }
 
 function CalendarDirective() {
-
+  return {
+    restrict: 'E',
+    scope: true,
+    templateUrl: 'cal.html',
+    transclude: true,
+    controller: function($scope){
+      $scope.$watchCollection('[calForm.year, calForm.month]', function(newVal, oldVal){
+        console.log(newVal, 'newVal');
+        var newDate = [newVal[0], parseFloat(newVal[1]) + 1];
+        console.log(newDate, 'newDate');
+        var dates = new Date(newDate);
+        console.log(dates, 'dates');
+        var monthlyRange = CalendarRange.getMonthlyRange(dates);
+        $scope.weekly = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+        $scope.days = monthlyRange;
+        console.log($scope);
+      });
+    }
+  }
 }
